90a5dcbfba533b3095fa3d1767e09888675e5293
BillaSpace <SmokenX1@gmail.com>
2025-09-03 23:54:12 +0530

    Update call.py

--- Opus/core/call.py
+++ Opus/core/call.py
@@ -575,6 +575,7 @@ class Call(PyTgCalls):
         return str(round(sum(pings) / len(pings), 3))
 
     async def start(self):
+        LOGGER(__name__).info("Starting PyTgCalls Client...\n")
         if config.STRING1:
             await self.one.start()
         if config.STRING2:
