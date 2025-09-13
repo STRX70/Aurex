3f9b5c6be32e030a271d431f6c053af95d2d21a2
STRX70 <salonisitole2305@gmail.com>
2025-09-13 12:11:12 +0530

    Update __init__.py

--- Opus/platforms/__init__.py
+++ Opus/platforms/__init__.py
@@ -6,14 +6,3 @@ from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
 from .Youtube import YouTubeAPI
 from .JioSavan import Saavn
-
-class PlaTForms:
-    def __init__(self):
-        self.apple = Apple()
-        self.carbon = Carbon()
-        self.saavn = Saavn()
-        self.resso = Resso()
-        self.soundcloud = SoundCloud()
-        self.spotify = Spotify()
-        self.telegram = Telegram()
-        self.youtube = YouTube()
